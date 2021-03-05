e3c2c32e78bad316bd51931612ff0b4386804bc3
Spolti <filippespolti@gmail.com>
2021-03-04 23:37:15 -0300

    Nexus OSS TrueNAS plugin - first implementation (#1)

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# iocage-nexus-oss
+# IOCage TrueNAS Nexus OSS Plugin
+
 TrueNAS IOCage Plugin for Nexus Repository OSS
+
+ - Nexus OSS download page: https://help.sonatype.com/repomanager3/download
+
+This repository contains all files needed to deploy it on TrueNAS.
+For more information about the purpose of each file, please take a look [here](https://www.truenas.com/docs/hub/contributing/creating-plugins/).
+
+
+## Installing manually
+
+This step might requires elevated privilege on TrueNAS Web Console.
+
+
+
+## Installing from TrueNAS Community Plugins
+
+TBA
+
+
+## Work In Progress
+
+This is a WIP plugin, and some features might not be available.
+ - At this point, it exposes only plain text http protocol
+ - No custom configuration for JVM, to do this edit this FILE manually and tune it to fit your needs.
+ - Default exposed port is 8081.
+
+Feel free to raise an issue [here](https://github.com/rebase-it/iocage-nexus-oss/issues/new/choose) if you find a bug or have suggestions for improvement.
+
+
+
--- post_install.sh
+++ post_install.sh
@@ -1,1 +1,53 @@
+#!/bin/sh
+# Rebasing XYZ :: Nexus OSS TrueNAS Plugin
+# 
+# Post install script, this script will>
+# - Download and copy the Nexus binary to its final location
+# - Unpack the file and remove the tar.gz file
+# - Create tne Nexus user
+# - Update the ownership on /home/nexus to the nexus user
+#
+#
+##########################################################
+# Local Envs
+# Default username
+USER="nexus"
+
+# Binary name
+BIN_NAME="nexus-oss.tar.gz"
+
+##########################################################
+# Add nexus user
+pw add user -n ${USER} -c NexusOSS -s /bin/sh -m
+
+##########################################################
+# Download and install Nexus
+fetch -o /home/nexus/${BIN_NAME} https://download.sonatype.com/nexus/3/latest-unix.tar.gz
+cd /home/nexus && tar -xzvf ${BIN_NAME}
+
+# remove the binary to save space
+rm -rfv ${BIN_NAME}
+
+# update ownership on nexus home
+chown -R ${USER}:${USER} /home/nexus
+
+##########################################################
+# Defines JAVA_HOME env
+export JAVA_HOME="/usr/local/openjdk8"
+
+##########################################################
+# Prepare nexus to run as a service
+mkdir -p /usr/local/etc/rc.d
+ln -s /home/nexus/nexus-3.30.0-01/bin/nexus /usr/local/etc/rc.d/
+sysrc -f /etc/rc.conf nexus_enable="YES"
+
+su - nexus -c 'service nexus start'
+
+##########################################################
+# Create the PLUGIN_INFO
+echo "Nexus OSS Plugin. For more info please visit https://github.com/rebase-it/iocage-nexus-oss.git" >> /root/PLUGIN_INFO
+
+##########################################################
+# Yei!!
+echo "Post install completed!"
 
